22febf47e89699091d319e8ad1e2ee8d8f7d2b0e
Youcef Benslimane <youyou.dbz@gmail.com>
2021-02-06 15:41:08 +0100

    Added Vector3

--- Over-Tracer/Vector3.cpp
+++ Over-Tracer/Vector3.cpp
@@ -2,152 +2,153 @@
 #include <math.h>
 #include <iostream>
 
+
+//Empty Constructor
 Vector3::Vector3() {
 	m_coordinates[0] = 0.0f;
 	m_coordinates[1] = 0.0f;
 	m_coordinates[2] = 0.0f;
 }
 
-
+//Constructor
 Vector3::Vector3(float x, float y, float z) {
 	m_coordinates[0] = x;
 	m_coordinates[1] = y;
 	m_coordinates[2] = z;
 }
 
-template<typename T>
-Vec3<T>::Vec3(const Vec3& v) {
-	m_data[0] = v[0];
-	m_data[1] = v[1];
-	m_data[2] = v[2];
+//Constructor copy
+Vector3::Vector3(const Vector3& v) {
+	m_coordinates[0] = v[0];
+	m_coordinates[1] = v[1];
+	m_coordinates[2] = v[2];
 }
 
 
-template<typename T>
-Vec3<T>& Vec3<T>::operator+(const Vec3<T>& v) {
+#pragma region Operators
+Vector3& Vector3::operator+(const Vector3& v) {
 
-	Vec3<T>* res = new Vec3<T>(); // empty
+	Vector3* res = new Vector3(); // empty
 
-	(*res)[0] = m_data[0] + v[0];
-	(*res)[1] = m_data[1] + v[1];
-	(*res)[2] = m_data[2] + v[2];
+	(*res)[0] = m_coordinates[0] + v[0];
+	(*res)[1] = m_coordinates[1] + v[1];
+	(*res)[2] = m_coordinates[2] + v[2];
 
 	return *res;
 }
 
-template<typename T>
-Vec3<T>& Vec3<T>::operator-(const Vec3<T>& v) {
-	Vec3<T>* res = new Vec3<T>(); // empty
 
-	(*res)[0] = m_data[0] - v[0];
-	(*res)[1] = m_data[1] - v[1];
-	(*res)[2] = m_data[2] - v[2];
+Vector3& Vector3::operator-(const Vector3& v) {
+	Vector3* res = new Vector3(); // empty
+
+	(*res)[0] = m_coordinates[0] - v[0];
+	(*res)[1] = m_coordinates[1] - v[1];
+	(*res)[2] = m_coordinates[2] - v[2];
 
 	return *res;
 }
 
-template<typename T>
-Vec3<T>& Vec3<T>::operator*(const float& k) {
+Vector3& Vector3::operator*(const float& k) {
 
-	Vec3<T>* res = new Vec3<T>(); // empty
+	Vector3* res = new Vector3(); // empty
 
-	(*res)[0] = m_data[0] * k;
-	(*res)[1] = m_data[1] * k;
-	(*res)[2] = m_data[2] * k;
+	(*res)[0] = m_coordinates[0] * k;
+	(*res)[1] = m_coordinates[1] * k;
+	(*res)[2] = m_coordinates[2] * k;
 
 	return *res;
 
 }
-template<typename T>
-Vec3<T>& Vec3<T>::operator/(const float& k) {
+
+Vector3& Vector3::operator/(const float& k) {
 
 	if (k == 0) {
 		return *this;
 	}
 
-	Vec3<T>* res = new Vec3<T>(); // empty
+	Vector3* res = new Vector3(); // empty
 
-	(*res)[0] = m_data[0] / k;
-	(*res)[1] = m_data[1] / k;
-	(*res)[2] = m_data[2] / k;
-
-	return *res;
-}
-template<typename T>
-Vec3<T>& Vec3<T>::operator+=(const Vec3<T>& v) {
-
-	m_data[0] += v[0];
-	m_data[1] += v[1];
-	m_data[2] += v[2];
-
-	return *this;
-
-}
-
-template<typename T>
-Vec3<T>& Vec3<T>::operator-=(const Vec3<T>& v) {
-
-	m_data[0] -= v[0];
-	m_data[1] -= v[1];
-	m_data[2] -= v[2];
-
-	return *this;
-
-}
-
-template<typename T>
-Vec3<T>& Vec3<T>::operator*=(const float& k) {
-
-	m_data[0] *= k;
-	m_data[1] *= k;
-	m_data[2] *= k;
-
-	return *this;
-
-}
-
-
-template<typename T>
-Vec3<T>& Vec3<T>::cross(const Vec3<T>& v) {
-
-	Vec3<T>* res = new Vec3<T>(); // empty
-
-	(*res)[0] = m_data[1] * v[2] - m_data[2] * v[1];
-	(*res)[1] = m_data[2] * v[0] - m_data[0] * v[2];
-	(*res)[2] = m_data[0] * v[1] - m_data[1] * v[0];
+	(*res)[0] = m_coordinates[0] / k;
+	(*res)[1] = m_coordinates[1] / k;
+	(*res)[2] = m_coordinates[2] / k;
 
 	return *res;
 }
 
+Vector3& Vector3::operator+=(const Vector3& v) {
+
+	m_coordinates[0] += v[0];
+	m_coordinates[1] += v[1];
+	m_coordinates[2] += v[2];
+
+	return *this;
+
+}
 
 
-template<typename T>
-float Vec3<T>::dot(const Vec3<T>& v) {
-	return m_data[0] * v[0] +
-		m_data[1] * v[1] +
-		m_data[2] * v[2];
+Vector3& Vector3::operator-=(const Vector3& v) {
+
+	m_coordinates[0] -= v[0];
+	m_coordinates[1] -= v[1];
+	m_coordinates[2] -= v[2];
+
+	return *this;
+
+}
+
+
+Vector3& Vector3::operator*=(const float& k) {
+
+	m_coordinates[0] *= k;
+	m_coordinates[1] *= k;
+	m_coordinates[2] *= k;
+
+	return *this;
+}
+
+float& Vector3::operator[](int index) {
+	return m_coordinates[index];
+}
+
+float Vector3::operator[](int index) const {
+	return m_coordinates[index];
+}
+
+#pragma endregion
+
+
+
+//Cross product
+Vector3& Vector3::cross(const Vector3& v) {
+
+	Vector3* res = new Vector3(); // empty
+
+	(*res)[0] = m_coordinates[1] * v[2] - m_coordinates[2] * v[1];
+	(*res)[1] = m_coordinates[2] * v[0] - m_coordinates[0] * v[2];
+	(*res)[2] = m_coordinates[0] * v[1] - m_coordinates[1] * v[0];
+
+	return *res;
 }
 
 
 
-template<typename T>
-T& Vec3<T>::operator[](int index) {
-	return m_data[index];
+//Dot product
+float Vector3::dot(const Vector3& v) {
+	return m_coordinates[0] * v[0] +
+		m_coordinates[1] * v[1] +
+		m_coordinates[2] * v[2];
 }
 
-template<typename T>
-T Vec3<T>::operator[](int index) const {
-	return m_data[index];
+
+
+
+
+float Vector3::norm() {
+	return sqrt(m_coordinates[0] * m_coordinates[0] +
+		m_coordinates[1] * m_coordinates[1] +
+		m_coordinates[2] * m_coordinates[2]);
 }
 
-template<typename T>
-float Vec3<T>::norm() {
-	return sqrt(m_data[0] * m_data[0] +
-		m_data[1] * m_data[1] +
-		m_data[2] * m_data[2]);
-}
-
-template<typename T>
-Vec3<T>& Vec3<T>::normalize() {
+Vector3& Vector3::normalize() {
 	return *this / this->norm();
 }
